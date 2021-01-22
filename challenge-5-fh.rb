33c313e69e9dad0c553b39c38972d41ec7e4b622
Niko Escobal <niko.escobal@gmail.com>
2021-01-22 23:13:05 +0800

    Simplified solution for challenge-5-fh.rb.

--- challenge-5-fh.rb
+++ challenge-5-fh.rb
@@ -1,14 +1,14 @@
 # 5. Develop a currency converter. The program should take two inputs: The exchange rate for the amount of your local currency per US Dollar and an amount of US Dollar. The output should be properly formatted with the correct currency units and display the equivalent amount of your local currency.
 
 
-def usd_to_peso(value)
-  usd = 48.07 * value
+def usd_to_peso(local)
+  usd = 48.07 * local
 end
 
-puts "enter dollar amount: "
+puts "enter value: "
 dollar_amount = gets.chomp.to_f
 
-puts "#{usd_to_peso(dollar_amount)} is the equivalent amount of pesos to dollars."
+puts "#{usd_to_peso(dollar_amount)} PHP is the equivalent amount of Philippine Pesos to US dollars."
 
 
 
