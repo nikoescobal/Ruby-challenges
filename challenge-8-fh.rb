457c81abeb30a605e6a46f69844d1cf0cbe671aa
Niko Escobal <niko.escobal@gmail.com>
2021-01-25 23:21:40 +0800

    Added another solution attempt to challenge-8-fh.rb.

--- challenge-8-fh.rb
+++ challenge-8-fh.rb
@@ -5,16 +5,47 @@ grade = gets.chomp.to_f
 
 # first attempt with nested if statements
 
-  # if grade < 0 || grade > 100
-  #   puts "This student's grade has been entered incorrectly."
-  # elsif grade >= 60 
-  #   puts "This student passed."
-  # elsif grade >= 50 && grade < 60
-  #   puts "This student needs to retake the exam."
-  # elsif grade < 50 
-  #   puts "This student has failed."
-  # end
+  if grade < 0 || grade > 100
+    puts "This student's grade has been entered incorrectly."
+  elsif grade >= 60 
+    puts "This student passed."
+  elsif grade >= 50 && grade < 60
+    puts "This student needs to retake the exam."
+  else 
+    puts "This student has failed."
+  end
 
   # second attempt with while loop
 
-  
+  # while !(grade < 0) || !(grade > 100)
+  #   if grade >= 60
+  #     puts "student has passed."
+  #   elsif grade > 50 && grade < 60
+  #        puts "student needs to rewrite the exam."
+  #   elsif grade < 50
+  #        puts "student has failed."
+  #   else
+  #     puts "student grade has been entered incorrectly."
+  #   end
+  # end
+
+# third attempt 
+
+  while (grade < 0) || (grade > 100)
+    puts "grade has been inputted incorrectly."
+    grade = gets.chomp
+
+    if grade >= 60
+      puts "student has passed."
+    elsif grade > 50 && grade < 60
+      puts "student needs to rewrite the exam."
+    else
+      puts "student has failed."
+    end
+  end
+
+
+
+    
+
+
