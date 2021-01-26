6c09822bca961af333976d0adb03a56570fb65f9
Niko Escobal <niko.escobal@gmail.com>
2021-01-26 17:39:27 +0800

    Added Chad's feedback on challenge-4-kk.rb.

--- challenge-4-kk.rb
+++ challenge-4-kk.rb
@@ -20,9 +20,9 @@ def total_sales(sales)
     gross_pay = 2000 + (0.15 * sales)
     deductions = 500 + (0.02 * sales)
     net_pay = gross_pay.to_f - deductions.to_f
-    puts "Total sales of the month : #{sales}"
-    puts "Gross pay of the month : #{gross_pay}"
-    puts "Net Pay of the month : #{net_pay}"
+    # puts "Total sales of the month : #{sales}"
+    # puts "Gross pay of the month : #{gross_pay}"
+    # puts "Net Pay of the month : #{net_pay}"
 end
 
 puts "Total sales of the month ?"
@@ -30,9 +30,15 @@ sales = gets.chomp.to_f
 total_sales(sales)
 
 
+# It's possible but it's a bad idea. Methods should folow the single responsibility principle, which means it does one thing only, so that it's more reusable. If you want to use methods more efficiently, then you're gonna have total_sales only calculate the total sales, and then you're gonna have a method called display_output, and have this pass through the sales, gross pay, and nett pay, and puts all of these within that method. - Chad-sensei
 
+def display_output(gross_pay, net_pay, total_sales)
+  puts "Total sales of the month : #{total_sales}"
+  puts "Gross pay of the month : #{gross_pay}"
+  puts "Net Pay of the month : #{net_pay}"
+end
 
-   
+# This is the more optimal solution.
 
 
 
--- challenge-8-fh.rb
+++ challenge-8-fh.rb
@@ -3,7 +3,6 @@
 puts "enter student's grade: "
 grade = gets.chomp.to_f
 
-# first attempt with nested if statements
 
   if grade < 0 || grade > 100
     puts "This student's grade has been entered incorrectly."
@@ -14,36 +13,6 @@ grade = gets.chomp.to_f
   else 
     puts "This student has failed."
   end
-
-  # second attempt with while loop
-
-  # while !(grade < 0) || !(grade > 100)
-  #   if grade >= 60
-  #     puts "student has passed."
-  #   elsif grade > 50 && grade < 60
-  #        puts "student needs to rewrite the exam."
-  #   elsif grade < 50
-  #        puts "student has failed."
-  #   else
-  #     puts "student grade has been entered incorrectly."
-  #   end
-  # end
-
-# third attempt 
-
-  while (grade < 0) || (grade > 100)
-    puts "grade has been inputted incorrectly."
-    grade = gets.chomp
-
-    if grade >= 60
-      puts "student has passed."
-    elsif grade > 50 && grade < 60
-      puts "student needs to rewrite the exam."
-    else
-      puts "student has failed."
-    end
-  end
-
 
 
     
