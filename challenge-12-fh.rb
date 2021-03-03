f39737c72fec42a4ab7df943af42afadc587e024
Niko Escobal <niko.escobal@gmail.com>
2021-03-04 05:56:37 +0800

    Added challenge-12-fh.rb

---
+++ challenge-12-fh.rb
@@ -0,0 +1,4 @@
+# 12. The Fibonacci series is a series of integers such that the first two integers are 0 and 1. Thereafter, every number in the series is the sum of its two predecessors. Write a program that will print all Fibonacci numbers less than 100, using
+# a loop
+# recursion
+
